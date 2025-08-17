31d850073b6df950b6a3befeed8267337d909c3a
Maanav54 <ponugotimaanavirao@gmail.com>
2025-08-17 08:34:49 +0530

    mvc

--- appserver/routes/index.js
+++ appserver/routes/index.js
@@ -1,9 +1,13 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express');  
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+var router = express.Router(); 
 
-module.exports = router;
+/* GET home page. */ 
+
+router.get('/', function(req, res, next) { 
+
+ res.render('index', { title: 'Express' }); 
+
+}); 
+
+module.exports=router; 
